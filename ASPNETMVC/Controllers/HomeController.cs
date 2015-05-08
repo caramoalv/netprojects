62c8b0f1c0799290db04d4c780e468100f09289f
caramoalv <caramoalv@gmail.com>
2015-05-08 20:22:16 +0200

    sda

--- ASPNETMVC/Controllers/HomeController.cs
+++ ASPNETMVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASPNETMVC.Models;
 
 namespace ASPNETMVC.Controllers
 {
@@ -10,7 +11,12 @@ namespace ASPNETMVC.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var persona = new Persona();
+            return View(persona);
+        }
+        [HttpPost]
+        public ActionResult Index(Persona p) {
+            return View(p);
         }
 
         public ActionResult About()
---
+++ ASPNETMVC/Models/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETMVC.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Saludo { get; set; }
+
+        public Persona()
+        {
+            Saludo = "Hola ";
+            Nombre = string.Empty;
+        }
+}
